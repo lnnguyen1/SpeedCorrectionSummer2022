2c6b5c7b080c783d97db8a671960cbc35593c0eb
alonsoM <amorales112@cps.edu>
2021-07-28 14:02:17 -0500

    Added encoder corrections

--- TimedRobot/src/main/java/frc/robot/Robot.java
+++ TimedRobot/src/main/java/frc/robot/Robot.java
@@ -50,6 +50,15 @@ public class Robot extends TimedRobot {
     public static int middle_state;
     public static int end_state;
 
+  //target variables
+
+    public static double leftEncoderTarget;
+    public static double rightEncoderTarget;
+    public boolean leftReachedTarget; 
+    public boolean rightReachedTarget;
+
+  
+
   @Override
   
   public void robotInit() {
@@ -66,8 +75,18 @@ public class Robot extends TimedRobot {
     
     /*button1 = new JoystickButton(driveStick, 1);*/
     state = 0;
-    middle_state = 1;
-    end_state = 2;
+    //middle_state = 1;
+    //end_state = 2;
+
+    //target init
+
+    leftEncoderTarget = 0;
+    rightEncoderTarget = 0;
+
+    leftReachedTarget = false; 
+    rightReachedTarget = false;
+
+
   }
 
   /**
@@ -95,21 +114,71 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     encoder_left.setPosition(0);
     encoder_right.setPosition(0);
+
+    //set the distance in feet
+   // setDistanceTarget(6);
+
+    System.out.println("Start");
+    System.out.println(encoder_left.getPosition() + "\t" + leftEncoderTarget);
   }
+
+    
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-
+    
+    
     SmartDashboard.putNumber("Encoder Position", encoder_left.getPosition());
     SmartDashboard.putNumber("Encoder Position", encoder_right.getPosition());
 
-    System.out.println(encoder_left.getPosition() + "\t" + encoder_right.getPosition());
 
-    startPath();
+    if (state == 0){
+     // System.out.println(state);
+      setDistanceTarget(6);
+      state = 1;
+      
+
+    }
+    if (state == 1){
+      //System.out.println(state);
+      forwardDrive(2); 
+      
+    }
+
+    if(state == 2){
+      
+      //setDistanceTarget(5);
+      //state = 3;
+      stopDrive();
+      
+    } /*
+    if (state == 3){
+      System.out.println("===========================");
+      forwardDrive(4);
+      //stopDrive();
+      //System.out.println(state);
+      
+      
+    }
+    /*
+    if (state == 4){
+      stopDrive();
+      System.out.println(state);
+    }
+    */
+  
+    //System.out.println(encoder_left.getPosition() + "\t" + encoder_right.getPosition());
+
+   
+    
+    //startPath();
+    //forwardDrive();
 
   }
-  public void startPath(){  
+
+
+  /*public void startPath(){  
 
 
     if (-encoder_left.getPosition() < 22.95 && encoder_right.getPosition() < 22.95){
@@ -123,6 +192,7 @@ public class Robot extends TimedRobot {
       //state = middle_state;
       middlePath();
     } 
+    forwardDrive();
 
     
     
@@ -152,23 +222,93 @@ public class Robot extends TimedRobot {
       stopDrive();
     }
   }
+  */
+
+  //target functions
+
+  public void setDistanceTarget(double distanceToTravel)
+    {
+
+      leftEncoderTarget = -(encoder_left.getPosition() + (distanceToTravel*4.7));
+      rightEncoderTarget = encoder_right.getPosition() + (distanceToTravel*4.7);
+
+    }
   
   //autonomous methods 
   //forward drive
-  public void forwardDrive() {
+  public void forwardDrive( int next_state) {
 
-      leftMotor1.set(-0.1);
+      /*leftMotor1.set(-0.1);
       leftMotor2.set(-0.1);
       rightMotor1.set(0.128);
       rightMotor2.set(0.128);
+    */
 
+    double adjustSpeed = (0);
+    double threshold = (3);
+    double leftEncoderCurr = encoder_left.getPosition();
+    double rightEncoderCurr =  encoder_right.getPosition();
+    double differenceOf_Encoders = -leftEncoderCurr - rightEncoderCurr;
+
+    // if (Math.abs(differenceOf_Encoders) > threshold)
+    // {
+    //   if (differenceOf_Encoders > (0)){
+    //     adjustSpeed = 0.05;
+    //   }
+    //   if (differenceOf_Encoders < (0)){
+    //     adjustSpeed = -0.05;
+    //   }
+    // }
+
+
+      //target code
+      leftReachedTarget = false;
+      rightReachedTarget = false;
+      System.out.println("R" + rightEncoderCurr + "," + rightEncoderTarget);
+      System.out.println("L" + leftEncoderCurr + "," + leftEncoderTarget);
+      System.out.println(adjustSpeed);
+
+      if (encoder_left.getPosition() > -leftEncoderTarget)
+      {
+        leftMotor1.set(-0.236 + adjustSpeed);
+        leftMotor2.set(-0.236 + adjustSpeed);
+        //System.out.println(leftReachedTarget);
+      }
+
+      else{
+        leftMotor1.set(0.0);
+        leftMotor2.set(0.0);
+        leftReachedTarget = true;
+       // System.out.println(leftReachedTarget);
+        //System.out.println("left position" + leftEncoderCurr);
+      }
+
+      if (encoder_right.getPosition() < rightEncoderTarget)
+      {
+        rightMotor1.set(0.2 - adjustSpeed);
+        rightMotor2.set(0.2 - adjustSpeed);
+        //System.out.println(rightReachedTarget);
+  
+      }
+      else{
+        rightMotor1.set(0.0);
+        rightMotor2.set(0.0);
+        rightReachedTarget = true;
+        //System.out.println(rightReachedTarget);
+        //System.out.println("right position" + rightEncoderCurr);
+      }
+      if(leftReachedTarget && rightReachedTarget){
+        state = next_state;
+      }
+      
   }
   //right turn
-  public void turnDrive(){
+ 
+   public void turnDrive(){
     leftMotor1.set(-0.24);
     leftMotor2.set(-0.24);
-    rightMotor1.set(0.05);
-    rightMotor2.set(0.05);
+    rightMotor1.set(-0.24);
+    rightMotor2.set(-0.24);
   }
 
   //stop drive
@@ -177,6 +317,10 @@ public class Robot extends TimedRobot {
     leftMotor2.set(0.0);
     rightMotor1.set(0.0);
     rightMotor2.set(0.0);
+
+    //encoder_left.setPosition(0);
+    //encoder_right.setPosition(0);
+
   }
 
   /** This function is called once when teleop is enabled. */
@@ -204,21 +348,22 @@ public class Robot extends TimedRobot {
     System.out.println(encoder_left.getPosition() + "\t" + encoder_right.getPosition());
 
     //button 2 to turn right
-    if(driveStick.getRawButton(2)){
+    /*if(driveStick.getRawButton(2)){
 
       turnDrive();
-      
+      forwardDrive(1);
+
 
     }
     //trigger to move forward
     else if  (driveStick.getRawButton(1)){
-      forwardDrive();
+      forwardDrive(1);
 
     }
     else{
-
+    */
     teleop_drive.arcadeDrive(driveStick.getY(), -driveStick.getX());
-    }
+    //}
     /*
     SmartDashboard.putNumber("Encoder Position", encoder_left.getPosition());
     SmartDashboard.putNumber("Encoder Position", encoder_right.getPosition());
